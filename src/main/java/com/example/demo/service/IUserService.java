c752b2f587aae7039fd4b24dc5b3d039f161b6c5
J-Hemantesh-010 <springlearning@gmail.com>
2022-01-19 15:10:35 +0530

    new development branch

---
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.UserRepository;
+import com.example.demo.entity.User;
+
+public class IUserService {
+	UserRepository userRepository;
+
+	public void saveUser(User user) {
+		
+	}
+
+}
